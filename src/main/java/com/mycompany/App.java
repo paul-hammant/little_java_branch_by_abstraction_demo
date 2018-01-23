3c95278a41aae1847945de15a493c0afc89d7690
Paul Hammant <paul@hammant.org>
2018-01-22 23:52:44 -0500

    Hair color added that's stringified (regretably in hindsight)

--- src/main/java/com/mycompany/App.java
+++ src/main/java/com/mycompany/App.java
@@ -2,13 +2,27 @@ package com.mycompany;
 
 import org.jooby.Jooby;
 
+import java.util.List;
+import java.util.Random;
+
+import static java.util.Arrays.asList;
+
 /**
- * @author jooby generator
+ * @author Paul Hammant DevOps, (c) 2018
  */
 public class App extends Jooby {
 
   {
-    get("/", () -> "Hello World!");
+    get("/color/hair.json", (req, rsp) -> {
+      rsp.status(200)
+              .type("application/json")
+              .send("{\"color\":\"" + getChangingHairColor() + "\"}");
+    });
+  }
+
+  private String getChangingHairColor() {
+    List<String> colors = asList("Blonde", "Brown", "Black", "Red");
+    return colors.get(new Random().nextInt(colors.size()));
   }
 
   public static void main(final String[] args) {
--- src/test/java/com/mycompany/AppTest.java
+++ src/test/java/com/mycompany/AppTest.java
@@ -1,42 +1,72 @@
 package com.mycompany;
 
-import static io.restassured.RestAssured.get;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertEquals;
-
+import org.hamcrest.core.AnyOf;
+import org.jooby.Request;
+import org.jooby.Response;
 import org.jooby.test.JoobyRule;
 import org.jooby.test.MockRouter;
 import org.junit.ClassRule;
 import org.junit.Test;
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.startsWith;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
- * @author jooby generator
+ * @author Paul Hammant DevOps, (c) 2018
  */
 public class AppTest {
 
   /**
-   * One app/server for all the test of this class. If you want to start/stop a new server per test,
+   * One app/server for all the tests in this class. If you want to start/stop a new server per test,
    * remove the static modifier and replace the {@link ClassRule} annotation with {@link Rule}.
    */
   @ClassRule
   public static JoobyRule app = new JoobyRule(new App());
 
+  /**
+   * A service test that uses RestAssured to
+   * check hair color functionality over HTTP
+   */
   @Test
-  public void integrationTest() {
-    get("/")
+  public void serviceTest() {
+    get("/color/hair.json")
         .then()
         .assertThat()
-        .body(equalTo("Hello World!"))
+        .body(startsWith("{\"color\":\""))
+        .body(endsWith("\"}"))
+        .body(specifiesAnyOfTheAllowedColors())
         .statusCode(200)
-        .contentType("text/html;charset=UTF-8");
+        .contentType("application/json;charset=UTF-8");
   }
 
+  private AnyOf<String> specifiesAnyOfTheAllowedColors() {
+    return anyOf(containsString("Blonde"), containsString("Brown"),
+            containsString("Black"), containsString("Red"));
+  }
+
+  /**
+   * A unit test that checks hair color
+   * functionality without HTTP or TCP/IP
+   */
   @Test
   public void unitTest() throws Throwable {
-    String result = new MockRouter(new App())
-        .get("/");
+    Response rsp = mock(Response.class);
+    when(rsp.status(200)).thenReturn(rsp);
+    when(rsp.type("application/json")).thenReturn(rsp);
 
-    assertEquals("Hello World!", result);
+    String result = new MockRouter(new App(), mock(Request.class), rsp)
+        .get("/color/hair.json");
+
+    assertThat(result, startsWith("{\"color\":\""));
+    assertThat(result, endsWith("\"}"));
+    assertThat(result, specifiesAnyOfTheAllowedColors());
+    verify(rsp).type("application/json");
+    verify(rsp).status(200);
   }
-
 }
